23f368f74ad01101b5e8fb5b853f1b43f41acf14
V.A.K.M.Nethmina <cst21022@std.uwu.ac.lk>
2026-04-23 13:51:22 +0530

    first commit

---
+++ src/main/java/com/example/Customer/management/system/backend/CustomerManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Customer.management.system.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerManagementSystemBackendApplication.class, args);
+	}
+
+}
